d070e57c25e4562d40b4f159645248cdb06a99cd
SuperGoldenMan <seananderson10@hotmail.co.uk>
2025-04-03 01:11:47 +0100

    fixing scripts

--- scripts/utils/pathHelpers.js
+++ scripts/utils/pathHelpers.js
@@ -5,15 +5,12 @@ export function getBaseAssetPath() {
       : 'assets/team-logos/';
   }
   
+  export function getTeamLogoPath(teamId) {
+    return `${getBaseAssetPath()}${teamId}.png`;
+  }
+  
   export function getBaseDataPath() {
     return window.location.hostname.includes('github.io')
       ? '/pistons-fansite/data/'
       : 'data/';
   }
-  
-  export function getTeamLogoPath(teamId) {
-    const isGitHubPages = window.location.hostname.includes('github.io');
-    const baseAssetPath = isGitHubPages ? '/pistons-fansite/assets/team-logos/' : '../assets/team-logos/';
-    return `${baseAssetPath}${teamId}.png`;
-  }
-  
